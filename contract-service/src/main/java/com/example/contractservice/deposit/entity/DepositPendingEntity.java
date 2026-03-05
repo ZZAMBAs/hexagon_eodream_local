9c6f7b9a66aac25b30de52911d6b3e785434ccce
ZZAMBAs <rksidksrksi@naver.com>
2026-03-05 17:37:52 +0900

    fix(contract): JPA Auditing 활성화

--- contract-service/src/main/java/com/example/contractservice/deposit/entity/DepositPendingEntity.java
+++ contract-service/src/main/java/com/example/contractservice/deposit/entity/DepositPendingEntity.java
@@ -4,6 +4,7 @@ import com.example.contractservice.deposit.common.DepositPendingStatus;
 import com.example.contractservice.deposit.domain.DepositPending;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
@@ -17,6 +18,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Getter
@@ -27,6 +29,7 @@ import org.springframework.data.annotation.LastModifiedDate;
         }
 )
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class DepositPendingEntity {
 
     @Id
