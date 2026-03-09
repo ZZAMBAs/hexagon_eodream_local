b2394371c3d57e3658789c5c007f5fa27e91a91d
ZZAMBAs <rksidksrksi@naver.com>
2026-03-09 15:40:32 +0900

    feat(contract): 비관적 락 조회 추가

--- contract-service/src/main/java/com/example/contractservice/deposit/repository/DepositJpaRepository.java
+++ contract-service/src/main/java/com/example/contractservice/deposit/repository/DepositJpaRepository.java
@@ -1,12 +1,23 @@
 package com.example.contractservice.deposit.repository;
 
 import com.example.contractservice.deposit.entity.DepositEntity;
+import jakarta.persistence.LockModeType;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
 public interface DepositJpaRepository extends JpaRepository<DepositEntity, Long> {
 
     Optional<DepositEntity> findByMemberCode(String memberCode);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("""
+        SELECT d
+        FROM DepositEntity d
+        WHERE d.memberCode = :memberCode
+    """)
+    Optional<DepositEntity> findByMemberCodeForUpdate(String memberCode);
 
     boolean existsByMemberCode(String memberCode);
 }
