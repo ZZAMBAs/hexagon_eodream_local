85ed00106d631ccf3274eef41f8ac400bbfffee5
ZZAMBAs <rksidksrksi@naver.com>
2026-03-16 00:31:37 +0900

    fix(contract): logback 설정 내 참조 수정 및 캐시 사이즈 제한

--- contract-service/src/main/java/com/example/contractservice/common/logging/CaffeineErrorDeduplicator.java
+++ contract-service/src/main/java/com/example/contractservice/common/logging/CaffeineErrorDeduplicator.java
@@ -5,11 +5,14 @@ import com.github.benmanes.caffeine.cache.Caffeine;
 import java.util.concurrent.TimeUnit;
 
 public class CaffeineErrorDeduplicator implements SlackErrorDeduplicator {
+
+    private static final int MAXIMUM_CACHE_SIZE = 1000;
     private final Cache<String, Boolean> cache;
 
     public CaffeineErrorDeduplicator(int ttlMillis) {
         cache = Caffeine.newBuilder()
                 .expireAfterWrite(ttlMillis, TimeUnit.MILLISECONDS)
+                .maximumSize(MAXIMUM_CACHE_SIZE)
                 .build();
     }
 
