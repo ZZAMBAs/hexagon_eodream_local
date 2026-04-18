81324ebd1ea738f26c575744e5ba06e3ea256a5e
ZZAMBAs <rksidksrksi@naver.com>
2026-04-19 00:03:06 +0900

    feat(contract): Step 중간 실패 방지 및 리스너로 Job 성공 여부 설정

--- contract-service/src/main/java/com/example/contractservice/contract/common/config/ContractJobConfig.java
+++ contract-service/src/main/java/com/example/contractservice/contract/common/config/ContractJobConfig.java
@@ -1,7 +1,9 @@
 package com.example.contractservice.contract.common.config;
 
+import com.example.contractservice.contract.common.config.listener.ContractFailedStepJobExecutionListener;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
@@ -23,8 +25,16 @@ public class ContractJobConfig {
     public Job statusChangeJob() {
         return new JobBuilder(CONTRACT_STATUS_CHANGE_JOB_NAME, jobRepository)
                 .start(contractToDoneBatchStep)
-                .next(contractToInProgressBatchStep)
-                .next(contractToCancelledBatchStep)
+                    .on("*").to(contractToInProgressBatchStep)
+                .from(contractToInProgressBatchStep)
+                    .on("*").to(contractToCancelledBatchStep)
+                .end()
+                .listener(contractJobExecutionListener())
                 .build();
+    }
+
+    @Bean
+    public JobExecutionListener contractJobExecutionListener() {
+        return new ContractFailedStepJobExecutionListener();
     }
 }
